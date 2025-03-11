0b2c6913d67568b6d790479102f5a6b113cd2e29
Dan Orjuela <dancid84@hotmail.com>
2025-03-11 05:05:24 -0500

    Se agregan Test Unitarios

---
+++ test/Models/TestResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Danorjuela.Utils.ResponseProcessor.Test.Models;
+public class TestResponse
+{
+
+    public int Property1 {get;set;}
+    public string Property2 { get; set; }
+}
---
+++ test/ResponseServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Danorjuela.Utils.ResponseProcessor.Exceptions;
+using Danorjuela.Utils.ResponseProcessor.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Danorjuela.Utils.ResponseProcessor.Test;
+[TestClass]
+public class ResponseServiceTest
+{
+
+    readonly IServiceProvider _services = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+       .ConfigureServices((hostContext, services) =>
+       {
+           services.AddResponseProcessorService();
+
+       }).Build().Services;
+
+
+
+
+    [TestMethod]
+    public void ProcessExceptionTest1()
+    {
+        // execute
+        var service = _services.GetRequiredService<IResponseService>();
+        try
+        {
+            throw new NotFoundException("Not Found Element");
+        }
+        catch (Exception ex)
+        {
+            var result =  service.ProcessResponse(ex);
+            // Verify
+            Assert.IsNotNull(result);
+
+
+
+
+        }
+    }
+
+
+}
